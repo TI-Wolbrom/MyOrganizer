317c3b1c040eb50e8c21ad17f41b3fc186a68d92
Xerpiu <polea2@interia.pl>
2017-09-27 20:41:27 +0200

    Pierwsza propozycja menu (raczej zostanie zmieniona)

--- src/konkurs/Main.java
+++ src/konkurs/Main.java
@@ -1,26 +1,29 @@
 package konkurs;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,800,600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Konkurs2");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(this.getClass().getResource("/resources/fxml/Base.fxml"));
+		
+		BorderPane pane = loader.load();
+		
+		Scene scene = new Scene(pane);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("[My Organizer] by 3TI Wolbrom");
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+		
 }
