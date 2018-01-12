71d63999f9523bc6525335dbaae52a11bc316de1
DeZet <dzpgamer@gmail.com>
2018-01-12 21:11:15 +0100

    chyba o to chodziło :D

--- src/konkurs/fx_contollers/PlanController.java
+++ src/konkurs/fx_contollers/PlanController.java
@@ -16,9 +16,8 @@ public class PlanController {
 	
 	// --------------------------------------------------------------------------------------------------------------------
 	
-	private static String[] tokens;
-	private static ArrayList<TextField> txtList = new ArrayList<TextField>();
-	private static boolean check = false;
+	private String[] tokens;
+	private ArrayList<TextField> txtList = new ArrayList<TextField>();
 	
 	// --------------------------------------------------------------------------------------------------------------------	
 	
@@ -230,146 +229,65 @@ public class PlanController {
 		
 		// wczytywanie stuffu do list 
 
-//		System.out.println(check);
-//		if(!check){
-//			
-//		loadPlan("res/plan.txt");
-//		
-//		txtList.add(txtPlanHourLesson1);
-//		txtList.add(txtPlanHourLesson2);
-//		txtList.add(txtPlanHourLesson3);
-//		txtList.add(txtPlanHourLesson4);
-//		txtList.add(txtPlanHourLesson5);
-//		txtList.add(txtPlanHourLesson6);
-//		txtList.add(txtPlanHourLesson7);
-//		txtList.add(txtPlanHourLesson8);
-//		txtList.add(txtPlanMondayLesson1);
-//		txtList.add(txtPlanMondayLesson2);
-//		txtList.add(txtPlanMondayLesson3);
-//		txtList.add(txtPlanMondayLesson4);
-//		txtList.add(txtPlanMondayLesson5);
-//		txtList.add(txtPlanMondayLesson6);
-//		txtList.add(txtPlanMondayLesson7);
-//		txtList.add(txtPlanMondayLesson8);
-//		txtList.add(txtPlanTuesdayLesson1);
-//		txtList.add(txtPlanTuesdayLesson2);
-//		txtList.add(txtPlanTuesdayLesson3);
-//		txtList.add(txtPlanTuesdayLesson4);
-//		txtList.add(txtPlanTuesdayLesson5);
-//		txtList.add(txtPlanTuesdayLesson6);
-//		txtList.add(txtPlanTuesdayLesson7);
-//		txtList.add(txtPlanTuesdayLesson8);
-//		txtList.add(txtPlanWednesdayLesson1);
-//		txtList.add(txtPlanWednesdayLesson2);
-//		txtList.add(txtPlanWednesdayLesson3);
-//		txtList.add(txtPlanWednesdayLesson4);
-//		txtList.add(txtPlanWednesdayLesson5);
-//		txtList.add(txtPlanWednesdayLesson6);
-//		txtList.add(txtPlanWednesdayLesson7);
-//		txtList.add(txtPlanWednesdayLesson8);
-//		txtList.add(txtPlanThursdayLesson1);
-//		txtList.add(txtPlanThursdayLesson2);
-//		txtList.add(txtPlanThursdayLesson3);
-//		txtList.add(txtPlanThursdayLesson4);
-//		txtList.add(txtPlanThursdayLesson5);
-//		txtList.add(txtPlanThursdayLesson6);
-//		txtList.add(txtPlanThursdayLesson7);
-//		txtList.add(txtPlanThursdayLesson8);
-//		txtList.add(txtPlanFridayLesson1);
-//		txtList.add(txtPlanFridayLesson2);
-//		txtList.add(txtPlanFridayLesson3);
-//		txtList.add(txtPlanFridayLesson4);
-//		txtList.add(txtPlanFridayLesson5);
-//		txtList.add(txtPlanFridayLesson6);
-//		txtList.add(txtPlanFridayLesson7);
-//		txtList.add(txtPlanFridayLesson8);
-//		
-//		check = true;
-//		}
-//
-//		System.out.println(check);
-//		System.out.println(txtList.size());
-//		
-//		for(int i=0;i<txtList.size();i++){
-//			System.out.println(tokens[i] + "AAAAA" + i);
-//			System.out.println(txtList.get(i));
-//			txtList.get(i).setText(tokens[i]);
-//		}
+		loadPlan("res/plan.txt");
 
-///////////////////////////////////////////////////////////////////////////
-//
-//		tyn na górze nie dzia³a(why ;-; ?), a tyn na dole tak i nwm ;-;
-//	
-///////////////////////////////////////////////////////////////////////////
-		
-		
-		if(!check){
-			loadPlan("res/plan.txt");
-			check=true;
+		txtList.add(txtPlanHourLesson1);
+		txtList.add(txtPlanHourLesson2);
+		txtList.add(txtPlanHourLesson3);
+		txtList.add(txtPlanHourLesson4);
+		txtList.add(txtPlanHourLesson5);
+		txtList.add(txtPlanHourLesson6);
+		txtList.add(txtPlanHourLesson7);
+		txtList.add(txtPlanHourLesson8);
+		txtList.add(txtPlanMondayLesson1);
+		txtList.add(txtPlanMondayLesson2);
+		txtList.add(txtPlanMondayLesson3);
+		txtList.add(txtPlanMondayLesson4);
+		txtList.add(txtPlanMondayLesson5);
+		txtList.add(txtPlanMondayLesson6);
+		txtList.add(txtPlanMondayLesson7);
+		txtList.add(txtPlanMondayLesson8);
+		txtList.add(txtPlanTuesdayLesson1);
+		txtList.add(txtPlanTuesdayLesson2);
+		txtList.add(txtPlanTuesdayLesson3);
+		txtList.add(txtPlanTuesdayLesson4);
+		txtList.add(txtPlanTuesdayLesson5);
+		txtList.add(txtPlanTuesdayLesson6);
+		txtList.add(txtPlanTuesdayLesson7);
+		txtList.add(txtPlanTuesdayLesson8);
+		txtList.add(txtPlanWednesdayLesson1);
+		txtList.add(txtPlanWednesdayLesson2);
+		txtList.add(txtPlanWednesdayLesson3);
+		txtList.add(txtPlanWednesdayLesson4);
+		txtList.add(txtPlanWednesdayLesson5);
+		txtList.add(txtPlanWednesdayLesson6);
+		txtList.add(txtPlanWednesdayLesson7);
+		txtList.add(txtPlanWednesdayLesson8);
+		txtList.add(txtPlanThursdayLesson1);
+		txtList.add(txtPlanThursdayLesson2);
+		txtList.add(txtPlanThursdayLesson3);
+		txtList.add(txtPlanThursdayLesson4);
+		txtList.add(txtPlanThursdayLesson5);
+		txtList.add(txtPlanThursdayLesson6);
+		txtList.add(txtPlanThursdayLesson7);
+		txtList.add(txtPlanThursdayLesson8);
+		txtList.add(txtPlanFridayLesson1);
+		txtList.add(txtPlanFridayLesson2);
+		txtList.add(txtPlanFridayLesson3);
+		txtList.add(txtPlanFridayLesson4);
+		txtList.add(txtPlanFridayLesson5);
+		txtList.add(txtPlanFridayLesson6);
+		txtList.add(txtPlanFridayLesson7);
+		txtList.add(txtPlanFridayLesson8);
+
+		for(int i=0;i<txtList.size();i++){
+			txtList.get(i).setText(tokens[i]);
 		}
-		
-		// GODZINY
-    	txtPlanHourLesson1.setText(tokens[0]);
-    	txtPlanHourLesson2.setText(tokens[1]);
-    	txtPlanHourLesson3.setText(tokens[2]);
-    	txtPlanHourLesson4.setText(tokens[3]);
-    	txtPlanHourLesson5.setText(tokens[4]);
-    	txtPlanHourLesson6.setText(tokens[5]);
-    	txtPlanHourLesson7.setText(tokens[6]);
-    	txtPlanHourLesson8.setText(tokens[7]);
-
-    	// Monday
-    	txtPlanMondayLesson1.setText(tokens[8]);
-    	txtPlanMondayLesson2.setText(tokens[9]);
-    	txtPlanMondayLesson3.setText(tokens[10]);
-    	txtPlanMondayLesson4.setText(tokens[11]);
-    	txtPlanMondayLesson5.setText(tokens[12]);
-    	txtPlanMondayLesson6.setText(tokens[13]);
-    	txtPlanMondayLesson7.setText(tokens[14]);
-    	txtPlanMondayLesson8.setText(tokens[15]);
-
-    	// Tuesday
-    	txtPlanTuesdayLesson1.setText(tokens[16]);
-    	txtPlanTuesdayLesson2.setText(tokens[17]);
-    	txtPlanTuesdayLesson3.setText(tokens[18]);
-    	txtPlanTuesdayLesson4.setText(tokens[19]);
-    	txtPlanTuesdayLesson5.setText(tokens[20]);
-    	txtPlanTuesdayLesson6.setText(tokens[21]);
-    	txtPlanTuesdayLesson7.setText(tokens[22]);
-    	txtPlanTuesdayLesson8.setText(tokens[23]);
-
-    	// Wednesday
-    	txtPlanWednesdayLesson1.setText(tokens[24]);
-    	txtPlanWednesdayLesson2.setText(tokens[25]);
-    	txtPlanWednesdayLesson3.setText(tokens[26]);
-    	txtPlanWednesdayLesson4.setText(tokens[27]);
-    	txtPlanWednesdayLesson5.setText(tokens[28]);
-    	txtPlanWednesdayLesson6.setText(tokens[29]);
-    	txtPlanWednesdayLesson7.setText(tokens[30]);
-    	txtPlanWednesdayLesson8.setText(tokens[31]);
-
-    	// Thursday
-    	txtPlanThursdayLesson1.setText(tokens[32]);
-    	txtPlanThursdayLesson2.setText(tokens[33]);
-    	txtPlanThursdayLesson3.setText(tokens[34]);
-    	txtPlanThursdayLesson4.setText(tokens[35]);
-    	txtPlanThursdayLesson5.setText(tokens[36]);
-    	txtPlanThursdayLesson6.setText(tokens[37]);
-    	txtPlanThursdayLesson7.setText(tokens[38]);
-    	txtPlanThursdayLesson8.setText(tokens[39]);
-
-    	// Friday
-    	txtPlanFridayLesson1.setText(tokens[40]);
-    	txtPlanFridayLesson2.setText(tokens[41]);
-    	txtPlanFridayLesson3.setText(tokens[42]);
-    	txtPlanFridayLesson4.setText(tokens[43]);
-    	txtPlanFridayLesson5.setText(tokens[44]);
-    	txtPlanFridayLesson6.setText(tokens[45]);
-    	txtPlanFridayLesson7.setText(tokens[46]);
-    	txtPlanFridayLesson8.setText(tokens[47]);
+		System.out.println(tokens.length);
+		System.out.println(txtList.size());
 	}
 	
-    public static void loadPlan(String path){
+    public void loadPlan(String path){
 		String file = Utils.loadFileAsString(path);
 		tokens = file.split("~");
 		System.out.println("AAAA");
