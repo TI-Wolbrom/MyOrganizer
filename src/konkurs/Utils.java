74fb69b06a17d7d703465613d1bfdb83a424803e
DeZet <dzpgamer@gmail.com>
2018-02-25 22:26:45 +0100

    TODO #Cofanie/Przywracanie zmian, jakiś sensowny default ustawienia xd

--- src/konkurs/Utils.java
+++ src/konkurs/Utils.java
@@ -3,7 +3,6 @@ package konkurs;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -62,25 +61,5 @@ public class Utils {
 		br.close();
 		
 		return result.toString();
-	}
-	
-	// --------------------------------------------------------------------------------------------------------------------
-
-	public static String loadFileAsString(String path){
-		StringBuilder builder = new StringBuilder();
-		
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(path));
-			String line;
-			while((line = br.readLine()) != null){
-				builder.append(line + "\n");
-			}
-			
-			br.close();
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-		System.out.println(builder.toString());
-		return builder.toString();
 	}
 }
--- src/konkurs/fx_contollers/PlanController.java
+++ src/konkurs/fx_contollers/PlanController.java
@@ -1,192 +1,89 @@
 package konkurs.fx_contollers;
 
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import konkurs.AppManager;
-import konkurs.Utils;
+import konkurs.RecordPlan;
 
 public class PlanController {
 
 	
 	// --------------------------------------------------------------------------------------------------------------------
 	
-	private String[] tokens;
-	private ArrayList<TextField> txtList = new ArrayList<TextField>();
+	ObservableList<RecordPlan> recordsFromFile = FXCollections.observableArrayList();
+	ObservableList<RecordPlan> allRecords;
 	
 	// --------------------------------------------------------------------------------------------------------------------	
 	
     public void PlanControler(){
     }
+
+
+    @FXML
+    private TableView<RecordPlan> tableView;
+
+    @FXML
+    private TableColumn<RecordPlan, String> tabColumn1;
+
+    @FXML
+    private TableColumn<RecordPlan, String> tabColumn2;
+
+    @FXML
+    private TableColumn<RecordPlan, String> tabColumn3;
+
+    @FXML
+    private TableColumn<RecordPlan, String> tabColumn4;
+
+    @FXML
+    private TableColumn<RecordPlan, String> tabColumn5;
+
+    @FXML
+    private TableColumn<RecordPlan, String> tabColumn6;
     
-	
-	@FXML
-    private Label lblPlanHour;
+    @FXML
+    private ContextMenu PlanContextMenu;
 
     @FXML
-    private TextField txtPlanHourLesson8;
+    private MenuItem cmItemSaveChanges;
 
     @FXML
-    private TextField txtPlanHourLesson7;
+    private MenuItem cmItemUndo;
 
     @FXML
-    private TextField txtPlanHourLesson6;
+    private MenuItem cmItemRedo;
 
     @FXML
-    private TextField txtPlanHourLesson5;
+    private MenuItem cmItemAddRow;
 
     @FXML
-    private TextField txtPlanHourLesson4;
+    private MenuItem cmItemDeleteRow;
 
     @FXML
-    private TextField txtPlanHourLesson1;
-
-    @FXML
-    private TextField txtPlanHourLesson3;
-
-    @FXML
-    private TextField txtPlanHourLesson2;
-
-    @FXML
-    private Label lblPlanMonday;
-
-    @FXML
-    private TextField txtPlanMondayLesson1;
-
-    @FXML
-    private TextField txtPlanMondayLesson2;
-
-    @FXML
-    private TextField txtPlanMondayLesson3;
-
-    @FXML
-    private TextField txtPlanMondayLesson4;
-
-    @FXML
-    private TextField txtPlanMondayLesson5;
-
-    @FXML
-    private TextField txtPlanMondayLesson6;
-
-    @FXML
-    private TextField txtPlanMondayLesson7;
-
-    @FXML
-    private TextField txtPlanMondayLesson8;
-
-    @FXML
-    private Label lblPlanTuesday;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson8;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson7;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson6;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson5;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson4;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson3;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson2;
-
-    @FXML
-    private TextField txtPlanTuesdayLesson1;
-
-    @FXML
-    private Label lblPlanWednesday;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson8;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson7;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson6;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson5;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson4;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson3;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson2;
-
-    @FXML
-    private TextField txtPlanWednesdayLesson1;
-
-    @FXML
-    private Label lblPlanThursday;
-
-    @FXML
-    private TextField txtPlanThursdayLesson8;
-
-    @FXML
-    private TextField txtPlanThursdayLesson7;
-
-    @FXML
-    private TextField txtPlanThursdayLesson6;
-
-    @FXML
-    private TextField txtPlanThursdayLesson5;
-
-    @FXML
-    private TextField txtPlanThursdayLesson4;
-
-    @FXML
-    private TextField txtPlanThursdayLesson3;
-
-    @FXML
-    private TextField txtPlanThursdayLesson2;
-
-    @FXML
-    private TextField txtPlanThursdayLesson1;
-
-    @FXML
-    private Label lblPlanFriday;
-
-    @FXML
-    private TextField txtPlanFridayLesson8;
-
-    @FXML
-    private TextField txtPlanFridayLesson7;
-
-    @FXML
-    private TextField txtPlanFridayLesson6;
-
-    @FXML
-    private TextField txtPlanFridayLesson5;
-
-    @FXML
-    private TextField txtPlanFridayLesson4;
-
-    @FXML
-    private TextField txtPlanFridayLesson3;
-
-    @FXML
-    private TextField txtPlanFridayLesson2;
-
-    @FXML
-    private TextField txtPlanFridayLesson1;
-
+    private MenuItem cmItemReturn;
+    
     @FXML
     private Button btnReturn;
 
@@ -195,102 +92,196 @@ public class PlanController {
 
     @FXML
     private Button btnReverseChanges;
-	
-	// --------------------------------------------------------------------------------------------------------------------
-
-    
     
     // --------------------------------------------------------------------------------------------------------------------
-    @FXML
+
+    // KOMBINACJE KLAWISZY
+
+    KeyCombination ctrlZ = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.SHORTCUT_DOWN);
+    KeyCombination ctrlY = new KeyCodeCombination(KeyCode.Y, KeyCodeCombination.SHORTCUT_DOWN);
+    KeyCombination ctrlN = new KeyCodeCombination(KeyCode.N, KeyCodeCombination.SHORTCUT_DOWN);
+    KeyCombination ctrlS = new KeyCodeCombination(KeyCode.S, KeyCodeCombination.SHORTCUT_DOWN);
+    KeyCombination delete = new KeyCodeCombination(KeyCode.DELETE);
+    KeyCombination esc = new KeyCodeCombination(KeyCode.BACK_SPACE);
+    
+    // --------------------------------------------------------------------------------------------------------------------
+
+	// EDYTOWANIE KOLUMN NA PODWÓJNYM KLIKNIÊCIU :D
+	
+	public void changeHourCellEvent(CellEditEvent editedCell){
+		RecordPlan recordSelected = tableView.getSelectionModel().getSelectedItem();
+		recordSelected.setHour(editedCell.getNewValue().toString());
+	}
+	
+	public void changeValueD1CellEvent(CellEditEvent editedCell){
+		RecordPlan recordSelected = tableView.getSelectionModel().getSelectedItem();
+		recordSelected.setValueD1(editedCell.getNewValue().toString());
+	}
+
+	public void changeValueD2CellEvent(CellEditEvent editedCell){
+		RecordPlan recordSelected = tableView.getSelectionModel().getSelectedItem();
+		recordSelected.setValueD2(editedCell.getNewValue().toString());
+	}
+
+	public void changeValueD3CellEvent(CellEditEvent editedCell){
+		RecordPlan recordSelected = tableView.getSelectionModel().getSelectedItem();
+		recordSelected.setValueD3(editedCell.getNewValue().toString());
+	}
+
+	public void changeValueD4CellEvent(CellEditEvent editedCell){
+		RecordPlan recordSelected = tableView.getSelectionModel().getSelectedItem();
+		recordSelected.setValueD4(editedCell.getNewValue().toString());
+	}
+
+	public void changeValueD5CellEvent(CellEditEvent editedCell){
+		RecordPlan recordSelected = tableView.getSelectionModel().getSelectedItem();
+		recordSelected.setValueD5(editedCell.getNewValue().toString());
+	}
+    
+    // --------------------------------------------------------------------------------------------------------------------
+    
+	@FXML
     void onBtnReturn(ActionEvent event) {
     	AppManager.getAppInstance().switchToMain();
     }
-
-	// --------------------------------------------------------------------------------------------------------------------
 
     @FXML
     void onBtnReverseChanges(ActionEvent event) {
     }
 
-	// --------------------------------------------------------------------------------------------------------------------
-	
     @FXML
     void onBtnSaveChanges(ActionEvent event) {
+    	savePlan("res/dataP.bin");
     }
-
-	// --------------------------------------------------------------------------------------------------------------------
-    // GETTERS
-    // --------------------------------------------------------------------------------------------------------------------
     
-    // --------------------------------------------------------------------------------------------------------------------
+	// DODAWANIE NOWEGO RZÊDU DO TABELI
     
-    @FXML
-	public void initialize() {
+	@FXML
+	void onItemAddRow(){
+		RecordPlan newRecordPlan = new RecordPlan();
 		
-		// wczytywanie stuffu do list 
-
-		loadPlan("res/plan.txt");
-
-		txtList.add(txtPlanHourLesson1);
-		txtList.add(txtPlanHourLesson2);
-		txtList.add(txtPlanHourLesson3);
-		txtList.add(txtPlanHourLesson4);
-		txtList.add(txtPlanHourLesson5);
-		txtList.add(txtPlanHourLesson6);
-		txtList.add(txtPlanHourLesson7);
-		txtList.add(txtPlanHourLesson8);
-		txtList.add(txtPlanMondayLesson1);
-		txtList.add(txtPlanMondayLesson2);
-		txtList.add(txtPlanMondayLesson3);
-		txtList.add(txtPlanMondayLesson4);
-		txtList.add(txtPlanMondayLesson5);
-		txtList.add(txtPlanMondayLesson6);
-		txtList.add(txtPlanMondayLesson7);
-		txtList.add(txtPlanMondayLesson8);
-		txtList.add(txtPlanTuesdayLesson1);
-		txtList.add(txtPlanTuesdayLesson2);
-		txtList.add(txtPlanTuesdayLesson3);
-		txtList.add(txtPlanTuesdayLesson4);
-		txtList.add(txtPlanTuesdayLesson5);
-		txtList.add(txtPlanTuesdayLesson6);
-		txtList.add(txtPlanTuesdayLesson7);
-		txtList.add(txtPlanTuesdayLesson8);
-		txtList.add(txtPlanWednesdayLesson1);
-		txtList.add(txtPlanWednesdayLesson2);
-		txtList.add(txtPlanWednesdayLesson3);
-		txtList.add(txtPlanWednesdayLesson4);
-		txtList.add(txtPlanWednesdayLesson5);
-		txtList.add(txtPlanWednesdayLesson6);
-		txtList.add(txtPlanWednesdayLesson7);
-		txtList.add(txtPlanWednesdayLesson8);
-		txtList.add(txtPlanThursdayLesson1);
-		txtList.add(txtPlanThursdayLesson2);
-		txtList.add(txtPlanThursdayLesson3);
-		txtList.add(txtPlanThursdayLesson4);
-		txtList.add(txtPlanThursdayLesson5);
-		txtList.add(txtPlanThursdayLesson6);
-		txtList.add(txtPlanThursdayLesson7);
-		txtList.add(txtPlanThursdayLesson8);
-		txtList.add(txtPlanFridayLesson1);
-		txtList.add(txtPlanFridayLesson2);
-		txtList.add(txtPlanFridayLesson3);
-		txtList.add(txtPlanFridayLesson4);
-		txtList.add(txtPlanFridayLesson5);
-		txtList.add(txtPlanFridayLesson6);
-		txtList.add(txtPlanFridayLesson7);
-		txtList.add(txtPlanFridayLesson8);
-
-		for(int i=0;i<txtList.size();i++){
-			txtList.get(i).setText(tokens[i]);
-		}
-		System.out.println(tokens.length);
-		System.out.println(txtList.size());
+		// DODAWANIE DO LISTY
+		
+		tableView.getItems().add(newRecordPlan);
 	}
 	
-    public void loadPlan(String path){
-		String file = Utils.loadFileAsString(path);
-		tokens = file.split("~");
-		System.out.println("AAAA");
+	// USUWANIE RZÊDÓW
+	
+	@FXML
+	void onItemDeleteRow(){
+		ObservableList<RecordPlan> selectedRows;
+		allRecords = tableView.getItems();
+		
+		selectedRows = tableView.getSelectionModel().getSelectedItems();
+		
+		allRecords.removeAll(selectedRows);
 	}
-    
+	
+	// --------------------------------------------------------------------------------------------------------------------
+
+//	private void KeyListener(KeyEvent e){
+//		if(e.getCode() == KeyCode.ESCAPE){
+//	    	AppManager.getAppInstance().switchToMain();
+//		}
+//	}
+	
+	// --------------------------------------------------------------------------------------------------------------------
+	
+	public void initialize() {
+		
+//		try {
+//			temp = Files.createTempFile("records", "res");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
+		
+		cmItemAddRow.setAccelerator(ctrlN);
+		cmItemSaveChanges.setAccelerator(ctrlS);
+		cmItemUndo.setAccelerator(ctrlZ);
+		cmItemRedo.setAccelerator(ctrlY);
+		cmItemReturn.setAccelerator(esc);
+		cmItemDeleteRow.setAccelerator(delete);
+		// USTAWIENIE KOLUMN :P
+		
+		tabColumn1.setCellValueFactory(new PropertyValueFactory<RecordPlan, String>("hour"));
+		tabColumn2.setCellValueFactory(new PropertyValueFactory<RecordPlan, String>("valueD1"));
+		tabColumn3.setCellValueFactory(new PropertyValueFactory<RecordPlan, String>("valueD2"));
+		tabColumn4.setCellValueFactory(new PropertyValueFactory<RecordPlan, String>("valueD3"));
+		tabColumn5.setCellValueFactory(new PropertyValueFactory<RecordPlan, String>("valueD4"));
+		tabColumn6.setCellValueFactory(new PropertyValueFactory<RecordPlan, String>("valueD5"));
+		
+		// WCZYTYWANIE DO KOLUMN
+		
+		tableView.setItems(readPlan("res/dataP.bin"));
+
+		// UMO¯LIWIENIE EDYTOWANIA
+		
+		tableView.setEditable(true);
+		tabColumn1.setCellFactory(TextFieldTableCell.forTableColumn());
+		tabColumn2.setCellFactory(TextFieldTableCell.forTableColumn());
+		tabColumn3.setCellFactory(TextFieldTableCell.forTableColumn());
+		tabColumn4.setCellFactory(TextFieldTableCell.forTableColumn());
+		tabColumn5.setCellFactory(TextFieldTableCell.forTableColumn());
+		tabColumn6.setCellFactory(TextFieldTableCell.forTableColumn());
+		
+		// UMO¯LIWIENIE PICKNIÊCIA KILKU RZÊDÓW NARAZ
+		tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		
+	}
+	
+	// --------------------------------------------------------------------------------------------------------------------
+	
+//	public ObservableList<RecordPlan> getRecords(){
+//		record.add(new RecordPlan("8:00 - 8:45", "PR", "AS", "ZE", "K", "<3"));
+//		record.add(new RecordPlan("8:01 - 8:45", "PRa", "S", "ZE", "K", "<3"));
+//		record.add(new RecordPlan("8:02 - 8:45", "PRas", "", "ZE", "K", "<3"));
+//		record.add(new RecordPlan("8:03 - 8:45", "PRasz", "", "E", "K", "<3"));
+//		record.add(new RecordPlan("8:04 - 8:45", "PRasze", "", "", "K", "<3"));
+//		record.add(new RecordPlan("8:05 - 8:45", "PRaszek", "", "", "", "<3"));
+//		record.add(new RecordPlan("8:06 - 8:45", "PRaszek<", "", "", "", "3"));
+//		record.add(new RecordPlan("8:07 - 8:45", "PRaszek<3", "", "", "", ""));
+//		
+//		return record;
+//	}
+	
+	private void savePlan(String file){
+		allRecords = tableView.getItems();
+		try {
+			FileOutputStream fos = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(new ArrayList<RecordPlan>(allRecords));
+			oos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static ObservableList<RecordPlan> readPlan(String file){
+		
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			List<RecordPlan> list = (List<RecordPlan>) ois.readObject();
+			ois.close();
+			
+			return FXCollections.observableList(list);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return FXCollections.emptyObservableList();
+	}
+	
+//	class EditingCell extends TableCell<RecordPlan, String>{
+//		
+//	}
+	
 }
+
