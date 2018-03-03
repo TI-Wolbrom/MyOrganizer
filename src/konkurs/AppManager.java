263cb0942d2f406eda69f84680512397395d2c03
Xerpiu <xerpiu@onet.pl>
2018-03-03 23:36:48 +0100

    zmiana wersji na 0.5.0b

--- src/konkurs/AppManager.java
+++ src/konkurs/AppManager.java
@@ -7,7 +7,7 @@ public class AppManager {
 
 	// --------------------------------------------------------------------------------------------------------------------
 	
-	public static final String VERSION = "0.3.5a";
+	public static final String VERSION = "0.5.0b";
 	public static String OS_NAME = System.getProperty("os.name").toLowerCase();
 	
 	// --------------------------------------------------------------------------------------------------------------------
